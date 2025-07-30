ffb20850b4975f0ed53e08b5d912761084080a1c
hodako <kbry12@naver.com>
2025-07-30 20:39:17 +0900

    chore: changed build.gradle.kts - added plugins will be used

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+    jacoco
+    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("com.linecorp.build-recipe-plugin") version "1.1.1"
+}
+
+buildscript {
+    repositories {
+        mavenLocal()
+        google()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
+}
+
+repositories {
+    mavenCentral()
+    google()
+}
