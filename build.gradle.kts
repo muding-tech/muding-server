a6a8425d18cebb620571c413cdafe0eeaf912a01
hodako <kbry12@naver.com>
2025-07-31 22:39:52 +0900

    chore: minorchanes in lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
     id("com.linecorp.build-recipe-plugin") version "1.1.1"
+
 }
 
 buildscript {
